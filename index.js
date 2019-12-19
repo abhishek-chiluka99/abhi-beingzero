3d59435441e85814e50bdd0ecc38b395861f93a0
abhishek-chiluka99 <chilukaabhishek9@gmail.com>
2019-12-19 14:48:55 +0530

    css updated again

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const port =process.env.PORT || 3000
 
 app.get('/', (req, res) => res.sendfile(__dirname+'/public/html/dest.html'))
+app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
